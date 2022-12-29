bd1e36fa064a13181ba64b4023b949fc767ba267
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-28 17:30:33 -0800

    Front-end
    design is improved in short URL box

--- Front-end/src/Components/ShortURLBox.js
+++ Front-end/src/Components/ShortURLBox.js
@@ -1,22 +1,30 @@
-import { Button, Card, Col, Row, Input, Typography } from "antd";
+import { Button, Card, Col, Row, Input, Typography, message } from "antd";
 import "../Styles/Styles.css";
 
-const { Text } = Typography;
+const { Text, Title } = Typography;
 
 const ShortURLBox = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+  const onCopyClicked = () => {
+    navigator.clipboard.writeText("Shorten URL2");
+    messageApi.info("URL copied");
+  };
   return (
     <Row justify="center">
+      {contextHolder}
       <Col xl={12} lg={12} md={18} sm={24} xs={24}>
         <Card>
-          <Text type="secondary">Shorten URL</Text>
+          <Title level={5}>Shorten URL</Title>
           <Input.Group compact>
             <Input
-              ini
+              defaultValue={"localhost:3000/tw392"}
               style={{
                 width: "calc(100% - 80px)",
               }}
             />
-            <Button type="primary">Copy</Button>
+            <Button type="primary" onClick={onCopyClicked}>
+              Copy
+            </Button>
           </Input.Group>
         </Card>
       </Col>
