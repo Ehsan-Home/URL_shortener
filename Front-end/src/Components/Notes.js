07d64d90ccfb557e34142d5b0382d521a4f9c1e4
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-28 18:04:45 -0800

    Front-end
    notes changed to cards

--- Front-end/src/Components/Notes.js
+++ Front-end/src/Components/Notes.js
@@ -1,19 +1,15 @@
-import { Alert } from "antd";
+import { Card } from "antd";
 
 const Notes = () => {
   return (
-    <Alert
-      message="Notes"
-      description={
-        <ul>
-          <li>
-            My name is Ehsan Ghasaei and this is the assessment for full-stack
-            software developer role @ Pani Energy
-          </li>
-        </ul>
-      }
-      type="info"
-    />
+    <Card>
+      <ul>
+        <li>
+          My name is Ehsan Ghasaei and this is the assessment for full-stack
+          software developer role @ Pani Energy
+        </li>
+      </ul>
+    </Card>
   );
 };
 
