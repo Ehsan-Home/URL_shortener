5fe4bb1a9235196235c65811f1a1b86b65a9b996
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-28 16:14:46 -0800

    Front-end
    URL input is completed

--- Front-end/src/Components/TextInputBox.js
+++ Front-end/src/Components/TextInputBox.js
@@ -1,5 +1,41 @@
+import { Button, Card, Col, Divider, Form, Input, Row } from "antd";
+
 const TextInputBox = () => {
-  return <div>Text input</div>;
+  const [form] = Form.useForm();
+  const onFinish = () => {
+    console.log("finished tapped");
+  };
+  return (
+    <Row justify="center">
+      <Col span={12}>
+        <Card>
+          <Form form={form} layout="vertical" onFinish={onFinish}>
+            <Form.Item
+              name="url"
+              rules={[
+                {
+                  required: true,
+                  message: "Please enter URL",
+                },
+              ]}
+            >
+              <Input.Group compact>
+                <Input
+                  placeholder="Enter your URL"
+                  style={{
+                    width: "calc(100% - 100px)",
+                  }}
+                />
+                <Button type="primary" htmlType="submit">
+                  Shorten
+                </Button>
+              </Input.Group>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default TextInputBox;
