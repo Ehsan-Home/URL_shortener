cdecf00c41eeab79eac2914af9e35dc6bc66fa58
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-28 17:17:30 -0800

    Front-end
    Short URL box designed

--- Front-end/src/Components/ShortURLBox.js
+++ Front-end/src/Components/ShortURLBox.js
@@ -1,5 +1,27 @@
+import { Button, Card, Col, Row, Input, Typography } from "antd";
+import "../Styles/Styles.css";
+
+const { Text } = Typography;
+
 const ShortURLBox = () => {
-  return <div>Short URL Box</div>;
+  return (
+    <Row justify="center">
+      <Col xl={12} lg={12} md={18} sm={24} xs={24}>
+        <Card>
+          <Text type="secondary">Shorten URL</Text>
+          <Input.Group compact>
+            <Input
+              ini
+              style={{
+                width: "calc(100% - 80px)",
+              }}
+            />
+            <Button type="primary">Copy</Button>
+          </Input.Group>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default ShortURLBox;
