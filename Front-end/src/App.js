3db7961df1514800c63763fb29ffd48923506c99
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-28 18:03:04 -0800

    Front-end
    body background color changed

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -4,22 +4,26 @@ import Notes from "./Components/Notes";
 import ShortURLBox from "./Components/ShortURLBox";
 import TextInputBox from "./Components/TextInputBox";
 
+import "./Styles/Styles.css";
+
 function App() {
   return (
-    <Row justify="center" gutter={[0, 24]}>
-      <Col xs={24} sm={24} md={18} lg={20} xl={20}>
-        <Header />
-      </Col>
-      <Col xs={24} sm={24} md={18} lg={20} xl={20}>
-        <TextInputBox />
-      </Col>
-      <Col xs={24} sm={24} md={18} lg={20} xl={20}>
-        <ShortURLBox />
-      </Col>
-      <Col xs={24} sm={24} md={18} lg={20} xl={20}>
-        <Notes />
-      </Col>
-    </Row>
+    <div className="background">
+      <Row justify="center" gutter={[0, 24]}>
+        <Col xs={24} sm={24} md={18} lg={20} xl={20}>
+          <Header />
+        </Col>
+        <Col xs={24} sm={24} md={18} lg={20} xl={20}>
+          <TextInputBox />
+        </Col>
+        <Col xs={24} sm={24} md={18} lg={20} xl={20}>
+          <ShortURLBox />
+        </Col>
+        <Col xs={24} sm={24} md={18} lg={20} xl={20}>
+          <Notes />
+        </Col>
+      </Row>
+    </div>
   );
 }
 
