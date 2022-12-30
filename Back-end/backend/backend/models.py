5056d964523af24cffb228ca3b91512486692d4c
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-30 12:11:13 -0800

    DB
    URL model created

---
+++ Back-end/backend/backend/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class URL(models.Model):
+    long_url = models.TextField()
+    unique_key = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'pk: {self.pk}, long url: {self.long_url}, key {self.unique_key}'
