49e04d79fa84aac4b820c48cbf13e2d74e299f3b
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-30 12:46:13 -0800

    URL request
    url request views and urls added

---
+++ Back-end/backend/backend/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+from .models import URL
+from .serializers import URLSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+
+
+@csrf_exempt
+@api_view(['POST'])
+def url(request):
+    print("in url request")
