bb318ea49934784b3fc4d2ea63628d428fee3b27
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-30 12:26:15 -0800

    Serializers
    Serializers class created

---
+++ Back-end/backend/backend/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import URL
+
+
+class URLSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = URL
+        fields = ['long_url', 'unique_key']
