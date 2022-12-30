50e1cc3c548d3e4ab15d032ab8a645a19d9cd0a6
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-30 12:19:31 -0800

    DB
    toString function added to model and project added to settings

--- Back-end/backend/backend/models.py
+++ Back-end/backend/backend/models.py
@@ -6,4 +6,4 @@ class URL(models.Model):
     unique_key = models.CharField(max_length=20)
 
     def __str__(self):
-        return f'pk: {self.pk}, long url: {self.long_url}, key {self.unique_key}'
+        return f'pk: {self.pk}, long url: {self.long_url}, key: {self.unique_key}'
