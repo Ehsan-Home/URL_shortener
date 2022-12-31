da5ed8db5c3fa67cdd8bc7f561962e4750281522
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-30 17:26:02 -0800

    DB
    save function removed since it was inefficient

--- Back-end/backend/backend/models.py
+++ Back-end/backend/backend/models.py
@@ -9,7 +9,7 @@ class URL(models.Model):
     def __str__(self):
         return f'pk: {self.pk}, long url: {self.long_url}, key: {self.unique_key}'
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        self.unique_key = str(self.id) + " Unique"
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #     self.unique_key = str(self.id) + " Unique"
+    #     super().save(*args, **kwargs)
