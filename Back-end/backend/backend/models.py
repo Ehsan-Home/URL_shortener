30eb69201e73eff163080d3492f0649055263830
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-30 19:00:13 -0800

    Cleanup

--- Back-end/backend/backend/models.py
+++ Back-end/backend/backend/models.py
@@ -8,8 +8,3 @@ class URL(models.Model):
 
     def __str__(self):
         return f'pk: {self.pk}, long url: {self.long_url}, key: {self.unique_key}'
-
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
-    #     self.unique_key = str(self.id) + " Unique"
-    #     super().save(*args, **kwargs)
