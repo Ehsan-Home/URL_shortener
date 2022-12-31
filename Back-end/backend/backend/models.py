b617e056fcadd11151263cc8897d0df7e97917d0
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-30 17:04:12 -0800

    DB
    id added as a bidAutoField and save is overriden

--- Back-end/backend/backend/models.py
+++ Back-end/backend/backend/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 
 class URL(models.Model):
+    id = models.BigAutoField(primary_key=True)
     long_url = models.URLField()
     unique_key = models.CharField(max_length=20)
 
     def __str__(self):
         return f'pk: {self.pk}, long url: {self.long_url}, key: {self.unique_key}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.unique_key = str(self.id) + " Unique"
+        super().save(*args, **kwargs)
