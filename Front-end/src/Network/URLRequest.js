32664880f0c5913a16c03d545668d92c3dce2774
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-30 17:50:52 -0800

    Front-end
    response object converted to string using stringify

--- Front-end/src/Network/URLRequest.js
+++ Front-end/src/Network/URLRequest.js
@@ -3,7 +3,9 @@ import Network from "./Setup";
 
 const URLRequest = (data) => {
   Network.post("url/", data)
-    .then((response) => console.log("response from url request" + response))
+    .then((response) => {
+      console.log("url in string: " + JSON.stringify(response.data));
+    })
     .catch((error) => console.log("error from url request" + error));
 };
 
