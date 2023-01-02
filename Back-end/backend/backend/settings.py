dd6fe358a640a7f649be70a1132cacd22b9a4316
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-02 12:31:42 -0800

    Docker
    docker setup done

--- Back-end/backend/backend/settings.py
+++ Back-end/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&bplactkjfkh!z_(a-pkk8hncax)k6o9nsc*+_ik@__+!_ynqh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
