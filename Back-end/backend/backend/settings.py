10635069e63f3931bd8bf4e213d7870b3da16530
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-02 19:20:32 -0800

    localhost added

--- Back-end/backend/backend/settings.py
+++ Back-end/backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'http://localhost:8000/']
 
 
 # Application definition
