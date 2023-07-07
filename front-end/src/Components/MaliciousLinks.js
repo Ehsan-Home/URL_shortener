c73e89ad4c34f8f6e2088b0332e34dceba48afbd
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-07-06 17:19:48 -0700

    link added to preview keyword

--- front-end/src/Components/MaliciousLinks.js
+++ front-end/src/Components/MaliciousLinks.js
@@ -1,4 +1,5 @@
 import { Alert, Typography } from "antd";
+import { Link } from "react-router-dom";
 
 const { Paragraph } = Typography;
 
@@ -12,7 +13,7 @@ const MaliciousLinksAlert = () => {
           When using URL shortener services, exercise caution as they can hide
           malicious destinations and make it difficult to assess website
           credibility. Be mindful of potential risks and check the destination
-          using our <a href="#">preview</a> section.
+          using our <Link to="/preview">preview</Link> section.
         </Paragraph>
       }
       banner
